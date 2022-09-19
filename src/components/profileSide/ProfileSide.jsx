dc62c1628b0cddd1818bea83acb63847e92fdf05
jack11223 <hashmiajith@gmail.com>
2022-09-19 22:11:13 +0530

    profile design added

---
+++ src/components/profileSide/ProfileSide.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import LogoSearch from '../logosearch/LogoSearch'
+import ProfileCard from '../profileCard/ProfileCard'
+import './ProfileSide.css'
+
+const ProfileSide = () => {
+  return (
+    <div className="ProfileSide">
+        <LogoSearch/>
+        <ProfileCard/>
+    </div>
+  )
+}
+
+export default ProfileSide
